6bfc680c1fd6a1a7b3f910ea32ca23193fb2bdf4
Xuping Feng <geophydogvon@gmail.com>
2018-06-05 13:41:48 +0800

    Update dist-time.c

--- src-code/dist-time.c
+++ src-code/dist-time.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "sacio.h"
 
+/*-----------------------------------function of remove "newline" or "space" characters--------------------------------*/
 void no_spa(char *ps){
         char *pt = ps;
         while ( *ps != '\0'  ){
@@ -13,7 +14,7 @@ void no_spa(char *ps){
         *pt = '\0';
 }
 
-
+/*-------------------------------------------------main program----------------------------------------------------------*/
 int main(int argc, char *argv[]) {
     int i, j, npts, begin_index;
     float *data, **amp, dist, t1, t2, time, b, e, delta, distmin = 180., distmax = 0.;
@@ -37,8 +38,10 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+/*-------------------------------------------------get parameters from command line--------------------------------------*/
     f = fopen(argv[1],"r"); t1 = atof(argv[2]); t2 = atof(argv[3]); fout = fopen(argv[4],"w"); fp = fopen("plot.sh","w");
 
+/*-----------------------read SAC format files to get "time", "gcarc" and "amplitude" information------------------------*/
     while ( fgets(ss,256,f) ) {
         no_spa(ss);
         data = read_sac(ss,&hd);
@@ -55,15 +58,17 @@ int main(int argc, char *argv[]) {
         }
         free(data);
     }
-
+/*------------------------------------------- plot shell script with GMT software----------------------------------------*/
     fprintf(fp,"gmt gmtset FONT_LABEL 20p,5,black\n");
     fprintf(fp,"gmt gmtset FONT_ANNOT_PRIMARY 15p,5,black\ngmt gmtset MAP_LABEL_OFFSET 0.5c\n");
+    fprintf(fp,"gmt gmtset MAP_ORIGIN_X 1.5i\ngmt gmtset MAP_ORIGIN_Y 1.5i\n\n");
     fprintf(fp,"peak=`minmax out.txt | awk '{print $7}' | awk -F\"[<//>]\" '{print $3}'`\n");
     fprintf(fp,"awk '{print $1,$2,$3/'''$peak'''}' %s > %s.tmp\n\n", argv[4], argv[4] );
     fprintf(fp,"gmt psxy -R%.2f/%.2f/%.3f/%.3f -JX8i/6i -K -T>plot.ps\n", t1, t2, distmin, distmax);
-    fprintf(fp,"gmt surface %s.tmp -R -I%.4f/%.4f -G%s.grd\n", argv[4], (t2-t1)/200, (distmax-distmin)/100,argv[4]);
+    fprintf(fp,"gmt surface %s.tmp -R -I%.4f/%.4f -G%s.grd\n", argv[4], (t2-t1)/100, (distmax-distmin)/80,argv[4]);
     fprintf(fp,"gmt grd2cpt %s.grd -Cpolar>tmp.cpt -Z\n",argv[4]);
-    fprintf(fp,"gmt grdimage %s.grd -R -J -K -O -Bx%.2ff%.2f+l\"Time(sec)\" -By%.3ff%.2f+l\"Distance(degree)\" -BWSen -Ctmp.cpt>>plot.ps\n\n",argv[4], (t2-t1)/10, (t2-t1)/50, (distmax-distmin)/10, (distmax-distmin)/50);
+    fprintf(fp,"gmt grdimage %s.grd -R -J -K -O -Bx%.2ff%.2f+l\"Time(sec)\" -By%.3ff%.2f+l\"Distance(degree)\" -BWSen -Ctmp.cpt>>plot.ps\n\n" \
+            ,argv[4], (t2-t1)/10, (t2-t1)/50, (distmax-distmin)/10, (distmax-distmin)/50);
     fprintf(fp,"gmt gmtset FONT_LABEL 25p,5,black\ngmt gmtset MAP_LABEL_OFFSET 0.25c\n\n");
     fprintf(fp,"gmt psscale -Ctmp.cpt -D8.5i/3i/12/0.5 -Bx0.2+l\"Normalized amplitude\" -K -O >> plot.ps\n");
     fprintf(fp,"gmt psxy -R -J -O -T>>plot.ps\n");
